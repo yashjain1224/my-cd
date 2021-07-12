dd8375a8e8ef4d9705604184d59847d254b5cb8e
Yash Jain <46293639+yashjain1224@users.noreply.github.com>
2021-07-12 15:23:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     """Return a friendly HTTP greeting."""
-    return 'Hello I like to make AI Apps'
+    return 'Hello Yash, Saket'
 
 @app.route('/name/<value>')
 def name(value):
